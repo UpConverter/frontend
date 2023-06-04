7913e26006d34af0cf8ff9b900648c59fe428e34
TimofeyTst <yarik.mist@gmail.com>
2023-06-04 11:24:40 +0300

    Change left side to simple header

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,76 +1,32 @@
 import { getRouteMain, getRouteSettings } from '@app/providers/AppRouter/model/constants/routes';
-import styles from '@components/Menu/Menu.module.css';
-import MenuIcon from '@mui/icons-material/Menu';
-import NoteAddIcon from '@mui/icons-material/NoteAdd';
-import SummarizeIcon from '@mui/icons-material/Summarize';
-import { Box, Button, Drawer, List, ListItem, ListItemButton, ListItemIcon } from '@mui/material';
-import type { FC, KeyboardEvent, MouseEvent } from 'react';
-import { useCallback, useState } from 'react';
+import { Settings } from '@mui/icons-material';
+import { Box, IconButton, Typography } from '@mui/material';
+import type { FC } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+import styles from './Menu.module.css';
 export const Menu: FC = () => {
-    const [state, setState] = useState(false);
     const navigate = useNavigate();
 
-    const toggleDrawer = useCallback(
-        (open: boolean) => (event: KeyboardEvent | MouseEvent) => {
-            if (event.type === 'keydown' && (event as KeyboardEvent).key === 'Tab') {
-                return;
-            }
-            setState(open);
-        },
-        []
-    );
     const onSettingsClick = () => {
         navigate(getRouteSettings());
     };
+
     const onMainClick = () => {
         navigate(getRouteMain());
     };
 
     return (
-        <>
-            <Button onClick={toggleDrawer(true)}>
-                <MenuIcon fontSize='large' />
-            </Button>
-            <Drawer
-                anchor='left'
-                open={state}
-                onClose={toggleDrawer(false)}
+        <Box className={styles.rowContainer}>
+            <IconButton onClick={onSettingsClick}>
+                <Settings className={styles.settingsIcon} />
+            </IconButton>
+            <Typography
+                className={styles.text}
+                onClick={onMainClick}
             >
-                <Box
-                    className={styles.box}
-                    role='presentation'
-                    onClick={toggleDrawer(false)}
-                    onKeyDown={toggleDrawer(false)}
-                >
-                    <List>
-                        <ListItem
-                            disablePadding
-                            onClick={onSettingsClick}
-                        >
-                            <ListItemButton className={styles.text}>
-                                <ListItemIcon>
-                                    <NoteAddIcon fontSize='large' />
-                                </ListItemIcon>
-                                Settings tst
-                            </ListItemButton>
-                        </ListItem>
-                        <ListItem
-                            disablePadding
-                            key='Report'
-                            onClick={onMainClick}
-                        >
-                            <ListItemButton className={styles.text}>
-                                <ListItemIcon>
-                                    <SummarizeIcon fontSize='large' />
-                                </ListItemIcon>
-                                FMN tst
-                            </ListItemButton>
-                        </ListItem>
-                    </List>
-                </Box>
-            </Drawer>
-        </>
+                FMN
+            </Typography>
+        </Box>
     );
 };
