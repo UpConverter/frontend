76e4538bd2abf8822062de93ba9bfc30c957461f
TimofeyTst <yarik.mist@gmail.com>
2023-07-24 16:25:13 +0300

    Change production port

--- src/api/getApiUrl.ts
+++ src/api/getApiUrl.ts
@@ -1,5 +1,5 @@
 import * as process from 'process';
 
 export const getApiUrl = (): string => {
-    return process.env.REACT_APP_DEV === 'dev' ? 'http://0.0.0.0:8000' : `https://${window.location.host}/api`;
+    return process.env.REACT_APP_DEV === 'dev' ? 'http://0.0.0.0:8000' : `http://0.0.0.0:9000`;
 };
